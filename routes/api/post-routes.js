0020c2a42898d516a75508052b860491022e021c
Angela Gola <angelagola.ko@gmail.com>
2022-06-26 11:44:23 -0400

    Working code

--- routes/api/post-routes.js
+++ routes/api/post-routes.js
@@ -1,18 +1,31 @@
 const router = require('express').Router();
 const sequelize = require('../../config/connection');
-const { Post, User, Vote } = require('../../models');
+const { Post, User, Comment, Vote } = require('../../models');
 
 // get all users
 router.get('/', (req, res) => {
+  console.log('======================');
   Post.findAll({
-    attributes: ['id', 'post_url', 'title', 'created_at'],
+    attributes: [
+      'id',
+      'post_url',
+      'title',
+      'created_at',
+      [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'), 'vote_count']
+    ],
     order: [['created_at', 'DESC']],
     include: [
       {
+        model: Comment,
+        attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+        include: {
+          model: User,
+          attributes: ['username']
+        }
+      },
+      {
         model: User,
-        attributes: ['id', 'post_url', 'title', 'created_at',
-    [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'), 'vote_count']
-]
+        attributes: ['username']
       }
     ]
   })
@@ -28,10 +41,22 @@ router.get('/:id', (req, res) => {
     where: {
       id: req.params.id
     },
-    attributes: ['id', 'post_url', 'title', 'created_at',
-[sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'), 'vote_count']
-],
+    attributes: [
+      'id',
+      'post_url',
+      'title',
+      'created_at',
+      [sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'), 'vote_count']
+    ],
     include: [
+      {
+        model: Comment,
+        attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+        include: {
+          model: User,
+          attributes: ['username']
+        }
+      },
       {
         model: User,
         attributes: ['username']
@@ -65,48 +90,14 @@ router.post('/', (req, res) => {
     });
 });
 
-//PUT /api/posts/update
-router.put('/upvote', (req,res) => {
-    //custom static method created in models/Post.js
-    Post.upvote(req.body, { Vote })
-    .then(updatedPostData => res.json(updatedPostData))
+router.put('/upvote', (req, res) => {
+  // custom static method created in models/Post.js
+  Post.upvote(req.body, { Vote, Comment, User })
+    .then(updatedVoteData => res.json(updatedVoteData))
     .catch(err => {
-        console.log(err);
-        res.status(400).json(err);
+      console.log(err);
+      res.status(500).json(err);
     });
-    // //create the vote
-    // Vote.create({
-    //     user_id: req.body.user_id,
-    //     post_id: req.body.post_id
-    //   }).then(() => {
-    //     //then find the post we just voted on
-    //     return Post.findOne({
-    //         where: {
-    //             id: req.body.post_id
-    //         },
-    //         attributes: [
-    //             'id',
-    //             'post_url',
-    //             'title',
-    //             'created_at',
-    //             // use raw mySQL aggregate function query to get a count of how many votes the post has
-    //             //and return it under the name `vote_count`
-    //             [
-    //                 sequelize.literal('(SELECT COUNT(*) FROM vote WHERE post.id = vote.post_id)'),
-    //                 'vote_count'
-    //             ]
-    //         ]
-    //     })
-    //     .then(dbPostData => res.json(dbPostData))
-    //     .catch(err => {
-    //         console.log(err);
-    //         res.status(400).json(err);
-    //     });
-    //   })
-
-
-    //     .then(dbPostData => res.json(dbPostData))
-    //     .catch(err => res.json(err));
 });
 
 router.put('/:id', (req, res) => {
@@ -133,7 +124,6 @@ router.put('/:id', (req, res) => {
     });
 });
 
-
 router.delete('/:id', (req, res) => {
   Post.destroy({
     where: {
@@ -154,4 +144,3 @@ router.delete('/:id', (req, res) => {
 });
 
 module.exports = router;
-
